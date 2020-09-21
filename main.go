0d9c57ad578cc26bb27b9eff7bdade07e225f08e
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-09-21 11:20:14 +0530

    rpc server initialization

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"log"
+	"net"
+	"search_searvice/grpc"
+
+	g "google.golang.org/grpc"
+)
+
 func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	s := grpc.Server{}
+
+	grpcServer := g.NewServer()
+
+	grpc.RegisterSearchServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
 }
