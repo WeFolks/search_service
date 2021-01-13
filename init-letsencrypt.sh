8aa70ce028d6f0a24996b192ea2a3b5b142957be
Vishwas Puri <vishwaspuri09@gmail.com>
2021-01-14 01:10:58 +0530

    Changed some urls

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(search.digitaldrive.live)
+domains=(ec2-13-233-134-12.ap-south-1.compute.amazonaws.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vishwaspuri09@gmail.com" # Adding a valid address is strongly recommended
