61922741aafac07e3505ffd98e88e2e7944545de
Vishwas Puri <vishwaspuri09@gmail.com>
2021-01-29 13:16:41 +0530

    Nginx updates+ES in docker volume

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(search.digitaldrive.live)
+domains=(search.locaid.in)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vishwaspuri09@gmail.com" # Adding a valid address is strongly recommended
